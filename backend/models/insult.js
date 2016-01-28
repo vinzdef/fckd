d0851ea0ce1137c81532036ed719521cfbd1f2e6
Vincent <ghzmdr@gmail.com>
2016-01-28 01:45:16 +0100

    Http links validation regex

--- backend/models/insult.js
+++ backend/models/insult.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose')
+var HTTPValidator = require('../validators/http-validator')
 
 var InsultSchema   = new mongoose.Schema({
     text: { type: String, required: true },
-	image: String,
+	image: {type: String, validate: {
+		validator: function (image) {
+			return HTTPValidator.test(image)
+		},
+		message: "Image data is not a link"
+	}
 	custom: Boolean
 })
 
