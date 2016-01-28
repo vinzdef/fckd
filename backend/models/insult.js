8acb084e32c5dc63cf238e2b0a94642df63f890e
Vincent <ghzmdr@gmail.com>
2016-01-28 02:05:44 +0100

    Add TODO

--- backend/models/insult.js
+++ backend/models/insult.js
@@ -1,15 +1,17 @@
 var mongoose = require('mongoose')
 var HTTPValidator = require('../validators/http-validator')
 
-var InsultSchema   = new mongoose.Schema({
+//TODO: Add a computed normalized version of the text as an unique property
+var InsultSchema = new mongoose.Schema({
     text: { type: String, required: true },
+	custom: Boolean,
+
 	image: {type: String, validate: {
 		validator: function (image) {
 			return HTTPValidator.test(image)
 		},
 		message: "Image data is not a link"
-	}
-	custom: Boolean
+	}}
 })
 
 module.exports = mongoose.model('Insult', InsultSchema)
